5f0d0d9788d9a4ac5a22fbb2f08e31a92c006204
killcodings <kontaktid@ya.ru>
2023-11-29 01:45:55 +0300

    feat: style header

---
+++ dist/analitics.729485d3f807cc3b9b4a.js
@@ -0,0 +1,2 @@
+console.log("analytics");
+//# sourceMappingURL=analitics.729485d3f807cc3b9b4a.js.map
--- dist/analitics.8efed0f12f351a92ee4b.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./analytics.js ***!
-  \**********************/
-console.log('analytics');
-/******/ })()
-;
-//# sourceMappingURL=analitics.8efed0f12f351a92ee4b.js.map
